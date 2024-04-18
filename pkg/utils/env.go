359ba597abbc01f9794ce5792f7c7fef7ce5be35
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-04-18 18:24:07 +0700

    Implemented utils.GetEnvVarValue()

---
+++ pkg/utils/env.go
@@ -0,0 +1,11 @@
+package utils
+
+import "os"
+
+func GetEnvVarValue(key string, allowEmpty bool) string {
+	value := os.Getenv(key)
+	if value == "" && !allowEmpty {
+		panic("'" + key + "' environment variable not set")
+	}
+	return value
+}

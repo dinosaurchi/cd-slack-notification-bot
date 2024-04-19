3a5529b2d75bcdc344386d76fdcf149ce08f0d46
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-04-19 12:57:03 +0700

    Improved Test_ParseGithubPRInfoFromPROpenedMessage

--- pkg/slack/github.utils_test.go
+++ pkg/slack/github.utils_test.go
@@ -2,8 +2,10 @@ package slack_test
 
 import (
 	"cd-slack-notification-bot/go/pkg/slack"
+	"cd-slack-notification-bot/go/pkg/utils"
 	"testing"
 
+	slackgo "github.com/slack-go/slack"
 	"github.com/stretchr/testify/require"
 )
 
@@ -58,4 +60,55 @@ func Test_ParseGithubPRInfoFromTitle(t *testing.T) {
 		require.NoError(t, err)
 		require.Nil(t, actual)
 	})
+}
+
+func Test_ParseGithubPRInfoFromPROpenedMessage(t *testing.T) {
+	messages := []slackgo.Message{}
+	err := utils.LoadFromFile("./responses/github.json", &messages)
+	require.NoError(t, err)
+	require.Len(t, messages, 7)
+
+	githubInfos := []*slack.GithubPRInfo{}
+	for _, message := range messages {
+		runID, err := slack.ParseGithubPRInfoFromPROpenedMessage(message)
+		require.NoError(t, err)
+		githubInfos = append(githubInfos, runID)
+	}
+	require.Len(t, githubInfos, len(messages))
+
+	expectedGithubInfos := []*slack.GithubPRInfo{
+		{
+			PRNumber:        615,
+			RepoName:        "go-backend",
+			RepoOwner:       "stablyio",
+			ThreadTimestamp: "1713476246.235249",
+			Timestamp:       "1713476246.235249",
+		},
+		{
+			PRNumber:        420,
+			RepoName:        "stably-ramp-frontend",
+			RepoOwner:       "stablyio",
+			ThreadTimestamp: "1713454012.666599",
+			Timestamp:       "1713454012.666599",
+		},
+		{
+			PRNumber:        6,
+			RepoName:        "terraform-trinity",
+			RepoOwner:       "stablyio",
+			ThreadTimestamp: "1713335235.568829",
+			Timestamp:       "1713335235.568829",
+		},
+		{
+			PRNumber:        432,
+			RepoName:        "stably-ramp-frontend",
+			RepoOwner:       "stablyio",
+			ThreadTimestamp: "1713320791.744569",
+			Timestamp:       "1713320791.744569",
+		},
+		nil,
+		nil,
+		nil,
+	}
+
+	require.Equal(t, expectedGithubInfos, githubInfos)
 }

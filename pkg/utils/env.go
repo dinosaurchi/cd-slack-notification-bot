4eb65109676ea773a9096543dc4c23031c602280
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-04-20 03:08:33 +0700

    Added Trim quote from both ends to avoid issues with string values for GetEnvVarValue()

--- pkg/utils/env.go
+++ pkg/utils/env.go
@@ -2,6 +2,7 @@ package utils
 
 import (
 	"os"
+	"strings"
 )
 
 func GetEnvVarValue(key string, allowEmpty bool) string {
@@ -9,5 +10,9 @@ func GetEnvVarValue(key string, allowEmpty bool) string {
 	if value == "" && !allowEmpty {
 		panic("'" + key + "' environment variable not set")
 	}
+	// Trim quote from both ends to avoid issues with string values
+	// (sometime they are included in quotes, sometimes not)
+	value = strings.Trim(value, "\"")
+	value = strings.Trim(value, "'")
 	return value
 }

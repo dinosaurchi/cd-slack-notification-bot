bd7d5dcec8b7f84aaedb761f272e753f5aaaa43c
Tinh-Chi TRAN <tinhchi.tran@gmail.com>
2024-04-19 12:23:22 +0700

    Moved aws.GetAWSCodeBuildRunID() -> slack.GetAWSCodeBuildRunID()

--- pkg/slack/codebuild.utils.go
+++ pkg/slack/codebuild.utils.go
@@ -127,3 +127,17 @@ func GetAWSCodeSuiteRunIDFromMessage(
 	}
 	return "", nil
 }
+
+// If the targetURL is a valid AWS CodeBuild URL, this function will return the run ID.
+// Otherwise, it will return an empty string.
+func GetAWSCodeBuildRunID(
+	targetURL string,
+) (string, error) {
+	// [^/]+ means any character except '/' and '+' means one or more
+	re := regexp.MustCompile(`amazon\.com/.*/builds/([^/]+)/view`)
+	match := re.FindStringSubmatch(targetURL)
+	if len(match) > 1 {
+		return match[1], nil
+	}
+	return "", nil
+}
